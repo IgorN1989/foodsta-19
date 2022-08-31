795f501d2c6d5a8608e826f06fdacad1c9e9e48f
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2022-08-31 17:54:06 +0300

    multi-button

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,12 +1,16 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
+    openModalBtn1: document.querySelector("[data-modal-open-header]"),
+    openModalBtn2: document.querySelector("[data-modal-open-hero]"),
+    openModalBtn3: document.querySelector("[data-modal-open-offerings]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
     body: document.querySelector("[data-page]"), 
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn1.addEventListener("click", toggleModal);
+  refs.openModalBtn2.addEventListener("click", toggleModal);
+  refs.openModalBtn3.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
